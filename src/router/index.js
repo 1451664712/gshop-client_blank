dcfa64cdeb7753302fc9b2af332133891104f0c2
songziyu <1451664712@qq.com>
2019-08-14 13:24:42 +0800

    ''

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../views/MSite/MSite'
+import Search from '../views/Search/Search'
+import Order from '../views/Order/Order'
+import Profile from '../views/Profile/Profile'
+import Login from '../views/Login/Login'
+
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [
+    // dynamic segments start with a colon
+    {
+      path: '/msite',
+      component: MSite,
+      meta:
+        {showFooter: true}
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {showFooter: true}
+    },
+    {path: '/order',
+      component: Order,
+      meta: {showFooter: true}
+    },
+    {path: '/profile',
+      component: Profile,
+      meta: {showFooter: true}
+    },
+    {path: '/login',
+      component: Login
+    }
+  ]
+})
